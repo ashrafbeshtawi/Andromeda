1c4683bc7c207a11b76051e1e360808286f5b4c5
Ashraf Beshtawi <ashraf.beshtawi@foo.ag>
2022-07-01 17:00:30 +0200

    testing

---
+++ test/TestSimple.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.14;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Adoption.sol";
+
+contract TestAdoption {
+  // The address of the adoption contract to be tested
+  Adoption adoption = Adoption(DeployedAddresses.Adoption());
+
+  // The id of the pet that will be used for testing
+  uint expectedPetId = 8;
+
+  // The expected owner of adopted pet is this contract
+  address expectedAdopter = address(this);
+
+}
