6caa10475bf74a6265da2d5aa433f60e5bf06ef9
Ashraf Beshtawi <ashraf.beshtawi@foo.ag>
2022-07-15 16:59:57 +0200

    functional js tests

--- test/TestSimple.sol
+++ test/TestSimple.sol
@@ -2,16 +2,15 @@ pragma solidity ^0.8.14;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
-import "../contracts/Adoption.sol";
+import "../contracts/Simple.sol";
 
-contract TestAdoption {
-  // The address of the adoption contract to be tested
-  Adoption adoption = Adoption(DeployedAddresses.Adoption());
+contract TestSimple {
+  // The address of the Simple contract to be tested
+  Simple simple = Simple(DeployedAddresses.Simple());
 
   // The id of the pet that will be used for testing
   uint expectedPetId = 8;
 
   // The expected owner of adopted pet is this contract
   address expectedAdopter = address(this);
-
 }
